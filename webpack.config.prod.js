ba093471a708150765020ea14c11fcf7b144e535
Devin Truong <detruong@ucdavis.edu>
2016-05-06 12:09:57 -0700

    fixed minified copy of development version of react error

---
+++ webpack.config.prod.js
@@ -0,0 +1,43 @@
+var webpack = require("webpack");
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/pictr.jsx",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js"
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  devtool: 'source-map',
+  resolve: {
+    extensions: ["", ".js", ".jsx"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: "node-loader"
+      }
+    ]
+  }
+};
