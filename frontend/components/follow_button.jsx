f4484c07b16d7494814ba851de53975b599181ee
Devin Truong <detruong@ucdavis.edu>
2016-05-06 11:28:52 -0700

    follow button refactoring

--- frontend/components/follow_button.jsx
+++ frontend/components/follow_button.jsx
@@ -5,11 +5,14 @@ var UserStore = require('../stores/user_store');
 module.exports = React.createClass({
 
   getInitialState: function () {
-    return {users: []};
+    return {
+      followerId: JSON.parse(localStorage.getItem('currentUser')).id,
+      currentUser: UserStore.find(JSON.parse(localStorage.getItem('currentUser')).id)
+    };
   },
 
   componentDidMount: function () {
-    this.userListener = UserStore.addListener(this.getUsers);
+    this.userListener = UserStore.addListener(this.getUser);
     if (UserStore.all().length === 0) {
       ClientActions.fetchUsers();
     }
@@ -19,8 +22,8 @@ module.exports = React.createClass({
     this.userListener.remove();
   },
 
-  getUsers: function () {
-    this.setState({ users: UserStore.all() });
+  getUser: function () {
+    this.setState({ currentUser: UserStore.find(this.state.followerId) });
   },
 
   handleFollow: function(e){
@@ -34,10 +37,8 @@ module.exports = React.createClass({
   handleUnfollow: function(e){
     var self = this;
     var relationshipId;
-    var followerId = JSON.parse(localStorage.getItem('currentUser')).id;
-    var currentUser = UserStore.find(followerId);
 
-    currentUser.followed_users.forEach(function(followed_user){
+    this.state.currentUser.followed_users.forEach(function(followed_user){
       if (followed_user.followed_id === self.props.user.id) {
         relationshipId = followed_user.id;
       }
@@ -47,19 +48,16 @@ module.exports = React.createClass({
   },
 
   render: function() {
-    var user = this.props.user;
     var self = this;
-    var followerId = JSON.parse(localStorage.getItem('currentUser')).id;
-    var currentUser = UserStore.find(followerId);
     var followButton = <div className="follow-button" onClick={this.handleFollow}>Follow</div>;
-    if (currentUser) {
-      currentUser.followed_users.forEach(function(followed_user){
-        if (followed_user.followed_id === user.id) {
+    if (this.state.currentUser) {
+      this.state.currentUser.followed_users.forEach(function(followed_user){
+        if (followed_user.followed_id === self.props.user.id) {
           followButton = <div className="unfollow-button" onClick={self.handleUnfollow}>Followed</div>;
           }
         });
 
-        if (currentUser.id === user.id) {
+        if (this.state.followerId === self.props.user.id) {
           followButton = <div/>;
         }
 
